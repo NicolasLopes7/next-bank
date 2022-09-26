512d0678f0c2bcdb4994c0ad3a44a1585dc3f787
Nicolas Lopes <nicolaslopesbusiness@gmail.com>
2022-09-26 12:58:20 +0200

    feat(auth): persist session

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,9 @@
-import axios from 'axios';
+import axios, { HeadersDefaults } from 'axios';
 
 export const api = axios.create({
   baseURL: 'https://sql-node-project.up.railway.app',
 });
+
+export interface CommonHeaderProps extends HeadersDefaults {
+  authorization: string;
+}
